63275fde5aeac421c79d9447b8a267ae570e8146
karan <karan26.ahuja@gmail.com>
2020-03-31 19:44:59 +0530

    add

--- hello.rb
+++ hello.rb
@@ -1,9 +1,44 @@
-require 'rubygems'
 require 'sinatra'
-
-
-
+require 'pg'
 get '/' do
-  content_type :json
-    { :key1 => 'values1', :key2 => 'values2' }.to_json
-end
+	json_string = "["
+  begin
+    conection = PG.connect :dbname => 'kwizto', :user => 'karan', :password => 'password1'
+    t_messages = conection.exec 'SELECT * FROM cards'
+    t_messages.each do |s_message|
+    	json_string += "{"
+    	json_string += "\""
+    	json_string += "text"
+    	json_string += "\""
+    	json_string += ":"
+    	json_string += "\""
+    	json_string += s_message['text']
+    	json_string += "\""
+    	json_string += ','
+    	json_string += "\""
+    	json_string += "serialnum"
+    	json_string += "\""
+    	json_string += ":"
+    	json_string += s_message['serialnum']
+    	json_string += ','
+    	json_string += "\""
+    	json_string += "viewtype"
+    	json_string += "\""
+    	json_string += ":"
+    	json_string += "\""
+    	json_string += s_message['viewtype']
+    	json_string += "\""
+    	json_string += "},"
+    	puts "------------------------"
+    	puts s_message['text']
+    	puts "------------------------"
+    end
+    json_string += "]"
+  rescue PG::Error => e
+    val_error = e.message 
+  ensure
+    conection.close if conection
+  end
+  json_string.gsub! '},]', '}]'
+  json_string
+  end
