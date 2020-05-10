2b9a986f65e9a860d931bd1ff920f6c87eac0ed2
karan <karan26.ahuja@gmail.com>
2020-05-10 21:59:03 +0530

    add

--- hello.rb
+++ hello.rb
@@ -174,7 +174,7 @@ get '/' do
        # DB = Sequel.connect(ENV['DATABASE_URL']) 
        # DB = Sequel.connect(ENV['DATABASE_URL'])
 # DB = Sequel.connect('postgres://postgres:password1@localhost/kwizto')
-    t_messages = mydbconnection.exec 'SELECT * FROM cards order by serialnum,card_id limit '+limit_number.to_s+' offset '+offsetval
+    t_messages = mydbconnection.exec 'SELECT * FROM cards where deck_id = '+params["deck_id"]+' order by serialnum,card_id limit '+limit_number.to_s+' offset '+offsetval
     t_messages.each do |s_message|
     # puts "------------------------------------loop"
     # DB.fetch("SELECT * FROM cards") do |s_message|
